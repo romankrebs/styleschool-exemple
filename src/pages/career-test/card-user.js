55e31e5750f75328ed30d2d79fb0e62d19dbc505
romankrebs <roman.s.krebs@gmail.com>
2020-07-01 19:31:00 +0300

    build code

--- src/pages/career-test/card-user.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Grid, Paper } from "@material-ui/core";
-
-function Card(props) {
-  return <div />;
-}
-
-export default Card;
--- src/pages/master-class/amoforms.js
+++
@@ -1,5 +0,0 @@
-var amo_forms_params = {
-  id: 461554,
-  hash: "dbeb87e8e0cbfe3288a6dcb0b95ade4e",
-  locale: "ru"
-};
